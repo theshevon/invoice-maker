1aec79a7b02d366020aeee8fb50f59ec3755243f
theshevon <shevonmendis98@gmail.com>
2021-01-17 02:45:03 +1100

    Added logging

--- app/modules/invoice_data_generator.py
+++ app/modules/invoice_data_generator.py
@@ -7,7 +7,7 @@
 from modules.util import to_datetime
 from datetime import datetime
 
-def generate_data(sheet_records, start_date, end_date, debug):
+def generate_data(sheet_records, start_date, end_date, logger):
     """
         Converts data read from a google sheet into a format that can be utilised when generating the invoices.
 
@@ -15,7 +15,7 @@ def generate_data(sheet_records, start_date, end_date, debug):
             sheet_records  (List): A list where each item is a dictionary representing a record from the spreadsheet
             start_date (datetime): Lower bound for record cut off
             end_date   (datetime): Upper bound for record cut off
-            debug          (bool): Denotes whether or not logging is required
+            logger       (Logger): Logger
 
         Returns:
             Dictionary: A dictionary containing the information that needs to be added to the PDF
@@ -23,15 +23,17 @@ def generate_data(sheet_records, start_date, end_date, debug):
 
     invoice_data = []
 
-    start_date = to_datetime("04-04-98")
-    end_date = to_datetime("10-10-29")
+    # filter records to only retain those within time bounds
     sheet_records = [record for record in sheet_records if start_date <= to_datetime(record["Date"]) <= end_date]
 
     for record in sheet_records:
 
         # only look at approved entries
         if (record["Approved"].lower() != "true"):
+            logger.info(f"Skipping unapproved record: { record }")
             continue
+
+        logger.info(f"Processing record: { record }")
 
         client_data = {}
         client_data["name"] = record["First Name"] + " " + record["Last Name"]
--- app/modules/util.py
+++ app/modules/util.py
@@ -5,7 +5,7 @@
 """
 
 from datetime import datetime, timedelta
-from common.constants import DEFAULT_TIME_PERIOD, OLDEST_START_DATE, DATE_STR_FORMAT_1
+from common.constants import DEFAULT_TIME_PERIOD, OLDEST_START_DATE, DATE_STR_FORMAT_1, DATE_STR_FORMAT_2
 
 def to_datetime(date_as_str):
     """
@@ -20,7 +20,20 @@ def to_datetime(date_as_str):
 
     return datetime.strptime(date_as_str, DATE_STR_FORMAT_1)
 
-def determine_date_bounds(start_date, end_date, time_period, debug):
+def to_date_string(date):
+    """
+        Converts a date object into a string.
+
+        Arguments:
+            date (datetime): The data as a date object
+        
+        Returns:
+            string: The date as a string
+    """
+
+    return date.strftime(DATE_STR_FORMAT_2)
+
+def determine_date_bounds(start_date, end_date, time_period, logger):
     """
         Determines the start and end dates for an invoice based on info presented through the command line
         arguments.
@@ -29,27 +42,37 @@ def determine_date_bounds(start_date, end_date, time_period, debug):
             start_date  (string): Lower bound for record cut off
             end_date    (string): Upper bound for record cut off
             time_period (string): Info that needs to be added to the PDF
-            debug         (bool): Denotes whether or not logging is required
+            logger      (Logger): Logger
 
         Returns:
             start_date     (datetime): Lower bound for record cut off
             end_date       (datetime): Upper bound for record cut off
     """
+
+    logger.info("Attempting to determine start and end dates based on supplied values: " + \
+                                f"start_date: { start_date}; end_date: { end_date }; time_period: { time_period }")
     
-    if not (start_date and end_date):
-        end_date = datetime.now()
-        if not time_period:
-            time_period = DEFAULT_TIME_PERIOD
-        start_date = end_date - timedelta(weeks=int(time_period))
-    elif start_date:
-        if time_period:
-            end_date = start_date + timedelta(weeks=int(time_period))
-        else:
+    try:
+        if not (start_date or end_date):
             end_date = datetime.now()
-    else:
-        if time_period:
+            if not time_period:
+                time_period = DEFAULT_TIME_PERIOD
             start_date = end_date - timedelta(weeks=int(time_period))
+        elif start_date:
+            start_date = to_datetime(start_date)
+            if time_period:
+                end_date = start_date + timedelta(weeks=int(time_period))
+            else:
+                end_date = datetime.now()
         else:
-            start_date = OLDEST_START_DATE
+            end_date = to_datetime(end_date)
+            if time_period:
+                start_date = end_date - timedelta(weeks=int(time_period))
+            else:
+                start_date = OLDEST_START_DATE
+
+        logger.info(f"Dates updated: start_date: { to_date_string(start_date) }; end_date: { to_date_string(end_date) }")
+    except:
+        logger.error("Error while detemining date bounds!", exc_info=True)
     
     return start_date, end_date
